4d1d5514ff7c82107f628bb045fe7ea0e2a4dbef
Harmaton <njagiiharmaton@gmail.com>
2023-01-06 13:26:45 +0300

    re-model

--- client/src/data/projects.js
+++ client/src/data/projects.js
@@ -2,32 +2,32 @@ const projects = [
     {
         "name": "Foodie",
         "stack": "Android",
-        "img": "",
+        "img": "images/foodie.jpg",
         "desc": "a mobile application that displays a list of recipes and gives details on preparations",
         "link": ""
     },
     {
         "name": "Breaking Bad",
         "stack": "Android",
-        "img": "",
+        "img": "images/breakingbad.jpeg",
         "desc": "a simple app to display breaking bad and better call saul movies"
     },
     {
         "name": "Silly Virtual assistant",
         "stack": "AI",
-        "img": "",
+        "img": "images/ai.jpg",
         "desc": "An AI assistant that uses speech recognition to listen to commands you give and gives responses"
     },
     {
         "name": "Bank (Fintech) api",
         "stack": "Kotlin Spring boot",
-        "img": "",
+        "img": "images/bank.jpg",
         "desc": "a back end application that deals with transactions and can be implemented in a fintech environment"
     },
     {
         "name": "FullStack E-commerce",
         "stack": "React, Node js, Mongodb",
-        "img": "",
+        "img": "images/",
         "desc": "an e-commerce site devloped for a client "
     }
 ];
